a7506b27f262b428903aa7483188ac2a5abb013e
jaimindp <jaiminpa@usc.edu>
2021-02-07 09:03:22 -0800

    started on tweet reader

--- detect_tweet.py
+++ detect_tweet.py
@@ -1,10 +1,12 @@
 import tweepy
 import json
 
+# Read keys
 f = open('../keys.json','r')
 api_keys = json.loads(f.read())
 f.close()
 twitter_keys = {'consumer_key':api_keys['twitter_keys']['consumer_key'],'consumer_secret':api_keys['twitter_keys']['consumer_secret'],'access_token_key':api_keys['twitter_keys']['access_token_key'],'access_token_secret': api_keys['twitter_keys']['access_token_secret']}
+
 
 # Test fastest way to detect tweet
 elon = ['elonmusk',44196397]
@@ -18,10 +20,22 @@ api = tweepy.API(auth)
 tweets = api.user_timeline(screen_name=elon[0], 
                            # 200 is the maximum allowed count
                            count=200,
-                           include_rts = False,
+                           include_rts = True,
                            # Necessary to keep full_text 
                            # otherwise only the first 140 words are extracted
                            tweet_mode = 'extended'
                            )
 
+
 print(tweets)
+
+
+tweets2 = tw.Cursor(api.search,
+              q=search_words,
+              lang="en",
+              since=date_since).items(5)
+
+
+for tweet in tweets:
+	print(tweet.text)
+
