892d0d29feccb54154510bfa1196b3a1a6a6e421
jaimindp <jaiminpa@usc.edu>
2021-02-08 11:14:33 -0800

    automated wait time before sellign

--- kraken_api.py
+++ kraken_api.py
@@ -1,6 +1,6 @@
 import json
-import requests
 import ccxt
+import time
 
 
 # Read keys
@@ -10,5 +10,19 @@ f.close()
 
 kraken_keys = {'api_key':api_keys['krakenapi_keys']['api_key'],'secret_key':api_keys['krakenapi_keys']['secret_key']}
 k = ccxt.kraken({'apiKey':kraken_keys['api_key'], 'secret':kraken_keys['secret_key']})
+
+# Hardcode for now
 pair = 'DOGE/BTC'
-# trade = k.create_order(pair,'market','buy',50)
+tousd = 'DOGE/USD'
+sleeptime = 2
+buy_amount = 50
+
+usdpair = k.fetchTicker(tousd)
+print('Buying {} {} at {:.8f} which is ${:.5f} '.format(buy_amount,pair,k.fetchTicker(pair)['bid'],(usdpair['bid']+usdpair['ask'])/2))
+# trade = k.create_order(pair,'market','buy',buy_amount)
+
+time.sleep(sleeptime)
+usdpair = k.fetchTicker(tousd)
+print('Selling {} {} at {:.8f} which is ${:.5f} '.format(buy_amount,pair,k.fetchTicker(pair)['ask'],(usdpair['bid']+usdpair['ask'])/2))
+# trade = k.create_order(pair,'market','sell',buy_amount)
+
